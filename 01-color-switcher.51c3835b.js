d4f8a5750973f820b57768f874373d973f4e9994
Dadayat <136356333+Dadayat@users.noreply.github.com>
2023-10-24 21:11:54 +0000

    Deploying to gh-pages from @ Dadayat/goit-js-hw-09@bf8ec420f7f82c66e3b0a72807c5566d2b560a8d 🚀

--- 01-color-switcher.51c3835b.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector("button[data-start]"),document.querySelector("button[data-stop]"),document.body][0].addEventListener("click",(function(){Date.now(),setInterval((function(){Date.now(),con}),1e3)}));
-//# sourceMappingURL=01-color-switcher.51c3835b.js.map
--- 01-color-switcher.74bd3d01.js
+++
@@ -1,2 +0,0 @@
-let t;({startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.body}).startBtn.addEventListener("click",(()=>{t=Date.now(),setInterval((()=>{Date.now();con}),1e3)}));
-//# sourceMappingURL=01-color-switcher.74bd3d01.js.map
