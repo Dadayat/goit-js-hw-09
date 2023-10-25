e4ac680004fea5ba0b16cc47052547393b6b9541
Dadayat <136356333+Dadayat@users.noreply.github.com>
2023-10-25 15:33:55 +0300

    Update-02timerjs

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,24 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+    datetimePicker: document.getElementById('datetime-picker'),
+    startBtn: document.querySelector('button[data-start]'),
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        const selectedDate = selectedDates[0];
+       if (selectedDate < new Date()) {
+        Notify.failure("Please choose a date in the future");
+        return;
+    }
+  },
+};
+
+const picker = flatpickr("#datetime-picker", options);
